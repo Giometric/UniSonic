47d36b5693c0b7447a937a4d8ce27915d251e425
Giovanni Sabella <gio.giometric@gmail.com>
2022-06-20 11:57:02 -0500

    Add moving platform prefabs, implement sinking on floating platforms

--- Assets/Scripts/Movement/MovingPlatform.cs
+++ Assets/Scripts/Movement/MovingPlatform.cs
@@ -6,6 +6,12 @@ namespace Giometric.UniSonic
     public class MovingPlatform : DynamicPlatform
     {
         [SerializeField]
+        [Tooltip("The distance this platform will sink down when a character lands on it.")]
+        private float sinkDistance = 4f;
+        [SerializeField]
+        [Tooltip("The speed, in units per second, at which this platform sinks down when a character lands on it, and raises back up when they jump off.")]
+        private float sinkSpeed = 20f;
+        [SerializeField]
         [Tooltip("The destination offest position of this platform, relative to where it is placed in the scene.")]
         private Vector2 destinationOffset = new Vector2(128f, 0f);
         [SerializeField]
@@ -15,7 +21,10 @@ namespace Giometric.UniSonic
         [Tooltip("How long the platform waits at its start and destination positions before moving again.")]
         private float waitDuration = 1.5f;
 
-        private Vector3 startPos, endPos;
+        private float sink = 0f;
+        private Vector3 startPos;
+        private Vector3 endPos;
+        private Vector3 currentPos;
         private float waitTimer = 0f;
         private bool movingBack = false;
 
@@ -37,30 +46,34 @@ namespace Giometric.UniSonic
         {
             startPos = transform.position;
             endPos = startPos + new Vector3(destinationOffset.x, destinationOffset.y);
+            currentPos = startPos;
         }
 
         protected override void TickMovement(float deltaTime)
         {
             base.TickMovement(deltaTime);
 
+            float sinkDest = attachedPlayers.Count > 0 ? sinkDistance : 0f;
+            sink = Mathf.MoveTowards(sink, sinkDest, sinkSpeed * deltaTime);
+
             if (waitTimer > 0f)
             {
                 waitTimer -= deltaTime;
-                if (waitTimer > 0f)
+            }
+
+            if (waitTimer <= 0f)
+            {
+                Vector3 dest = movingBack ? startPos : endPos;
+                currentPos = Vector3.MoveTowards(currentPos, dest, moveSpeed * deltaTime);
+                if (Vector3.SqrMagnitude(dest - currentPos) <= 0.001f)
                 {
-                    // Still waiting, no movement
-                    return;
+                    currentPos = dest;
+                    movingBack = !movingBack;
+                    waitTimer = waitDuration;
                 }
             }
 
-            Vector3 dest = movingBack ? startPos : endPos;
-            transform.position = Vector3.MoveTowards(transform.position, dest, moveSpeed * deltaTime);
-            if (Vector3.SqrMagnitude(dest - transform.position) <= 0.001f)
-            {
-                transform.position = dest;
-                movingBack = !movingBack;
-                waitTimer = waitDuration;
-            }
+            transform.position = currentPos - new Vector3(0f, sink, 0f);
         }
     }
 }
